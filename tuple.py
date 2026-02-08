05a1589709dca9521260d189a635f78d95a95f97
ABRACADABRA-theoneandonly <dr.aaisharehman@gmail.com>
2026-02-08 14:50:10 +0000

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,15 @@
+#TUPLES
+tuple=("hi","¬*@{#~",3.1415926535897932384626433832795028841971693993751058209749445923078164,777,True)
+print(tuple)
+tupletuple=(234567890987665434567890009876543456789987654567898765434567898765434567,["qwerty","iuytrew","iuytrewertyui"],["ertyu","ytrtyui","wasd"])
+print (tupletuple)
+print(tupletuple[2][2][2])
+#slicinggggggggggggggggggggggggg
+tupletupletuple=(4567,8765,678,6543,345678,876543,45678,3.1415926535897932384626433832795028841971693993751058209749445923078164)
+print(tupletupletuple[0:3])
+print(tupletupletuple[5:])
+print(tupletupletuple[-3:])
+print(tupletupletuple[:])
+print(tupletupletuple[::-1])
+print(":)","(@.@)")
+tupletupletuple[1]=2345677654322222223456776543
